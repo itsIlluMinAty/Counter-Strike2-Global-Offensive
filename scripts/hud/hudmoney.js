6ad47a05eba8575b9022a50cab9b1aeac3353850
IlluMinAty <illuminatyuwu@hotmail.com>
2026-07-29 20:29:16 +0200

    legacy cs2go pb money fix

--- scripts/hud/hudmoney.js
+++ scripts/hud/hudmoney.js
@@ -1,38 +1,20 @@
 "use strict";
-
+/// <reference path="../csgo.d.ts" />
+/// <reference path="../digitpanel.ts" />
 var MoneyPanel;
 (function (MoneyPanel) {
-    const _elContainer = $('#jsRotaryMoney');
-    let _lastMoneyValue = -1; // all this does is check the last money value, if it's unchanged.. don't change it!
-
-    function _Init() {
-        if (_elContainer) {
-            const maxLen = "$16000"; 
-            DigitPanelFactory.MakeDigitPanel(_elContainer, maxLen.length, '', 0.6, "#buymenu_money_digitpanel_digits");
-            
-            _WatchMoney(); // observer loop for the mula.
-        }
-    }
-
-    function _WatchMoney() { 
-        const currentMoney = GameStateAPI.GetPlayerMoney(GameStateAPI.GetLocalPlayerXuid());
-
-        if (currentMoney !== _lastMoneyValue) {
-            _UpdateMoney(currentMoney, _lastMoneyValue === -1); 
-            _lastMoneyValue = currentMoney;
-        }
-        $.Schedule(0.1, _WatchMoney);
-    }
-
+    $.RegisterEventHandler('UpdateHudMoney', $.GetContextPanel(), _UpdateMoney);
     function _UpdateMoney(amt, bInstant = false) {
-        if (!_elContainer) return;
-
-        $.GetContextPanel().SetDialogVariableInt("money", amt);
-        const digitString = $.Localize("#buymenu_money", $.GetContextPanel());
-        
-        DigitPanelFactory.SetDigitPanelString(_elContainer, digitString, bInstant);
+        const elContainer = $('#jsRotaryMoney');
+        if (elContainer) {
+            if (!$('#DigitPanel')) {
+                $.GetContextPanel().SetDialogVariableInt("money", 16000);
+                const maxLen = $.Localize("#buymenu_money", $.GetContextPanel());
+                DigitPanelFactory.MakeDigitPanel(elContainer, maxLen.length, '', 0.6, "#buymenu_money_digitpanel_digits");
+            }
+            $.GetContextPanel().SetDialogVariableInt("money", amt);
+            const digitString = $.Localize("#buymenu_money", $.GetContextPanel());
+            DigitPanelFactory.SetDigitPanelString(elContainer, digitString, bInstant);
+        }
     }
-
-    _Init();
-
 })(MoneyPanel || (MoneyPanel = {}));
